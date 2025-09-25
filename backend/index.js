9879fc25e16a9480e651e9b35b1c8c8e1e025021
Samir warsi <samir.warsi2001@gmail.com>
2025-09-25 21:52:10 +0530

    adding

--- backend/index.js
+++ backend/index.js
@@ -16,13 +16,19 @@ app.use(cors({
     credentials: true
 }))
 
+app.use(express.static(path.join(__dirname,"frontend","dist")))
 
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
 
 app.use("/api/v1/admin", adminRoute)
 app.use("/api/v1/partner", partnerRoute)
 app.use("/api/v1/register", registerRoute)
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend','dist', 'index.html'))
+})
 
 
 const main =()=>{
