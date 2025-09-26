6be7d5105ead3aeb065009ef28c79934d1ad4452
Samir warsi <samir.warsi2001@gmail.com>
2025-09-26 17:12:41 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const path = require("path")
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }))
 
